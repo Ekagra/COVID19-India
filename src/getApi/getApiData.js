416b17807de39170f6faf83c06f454bdb1cbe7a7
Ekagra <ekagra001@gmail.com>
2021-09-04 12:09:27 +0530

    fixed api issue by removing deprecated api call and using the latest one

--- src/getApi/getApiData.js
+++ src/getApi/getApiData.js
@@ -2,8 +2,12 @@ import React, {useState,useEffect} from 'react'
 import Card from "../Cards/Cards"
 import ReactMapGL, {Marker} from "react-map-gl";
 import styles from "./getApiData.css";
+import mapboxgl from "mapbox-gl";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 const Data = () => {
+
 
     const[viewport,setViewport] = useState({
         longitude: 78.0792,
@@ -13,46 +17,48 @@ const Data = () => {
         zoom: 5,
     })
 
-    var act = 0;
+    var conf = 0;
     var rec =0;
     var dec =0;
 
-    const states = [{state:"Andhra Pradesh",lng:15.91,lat:79.74},
-        {state:"Arunachal Pradesh",lng:28.21,lat:94.72},
-        {state:"Assam",lng:26.20,lat:92.93},
-        {state:"Bihar",lng:25.09,lat:85.31},
-        {state:"Chhattisgarh",lng:21.27,lat:81.86},
-        {state:"Goa",lng:15.29,lat:74.12},
-        {state:"Gujarat",lng:22.25,lat:71.19},
-        {state:"Haryana",lng:29.05,lat:76.08},
-        {state:"Himachal Pradesh",lng:31.10,lat:77.17},
-        {state:"Jammu and Kashmir",lng:33.77 ,lat:76.57},
-        {state:"Jharkhand",lng:23.61,lat:85.27},
-        {state:"Karnataka",lng:15.31,lat:75.71},
-        {state:"Kerala",lng:10.85,lat:76.27},
-        {state:"Madhya Pradesh",lng:22.97,lat:78.65},
-        {state:"Maharashtra",lng:19.75,lat:75.71},
-        {state:"Manipur",lng:24.66,lat:93.90},
-        {state:"Meghalaya",lng:25.46,lat:91.36},
-        {state:"Mizoram",lng:23.16,lat:92.93},
-        {state:"Nagaland",lng:26.15,lat:94.56},
-        {state:"Odisha",lng:20.95,lat:85.09},
-        {state:"Punjab",lng:31.14,lat:75.34},
-        {state:"Rajasthan",lng:27.02,lat:74.21},
-        {state:"Sikkim",lng:27.53,lat:88.51},
-        {state:"Tamil Nadu",lng:11.12,lat:78.65},
-        {state:"Tripura",lng:23.94,lat:91.98},
-        {state:"Uttarakhand",lng:30.06,lat:79.01},
-        {state:"Uttar Pradesh",lng:26.84,lat:80.94},
-        {state:"West Bengal",lng:22.98,lat:87.85}]
+
+    const states = [{state:"AP",lng:15.91,lat:79.74},
+        {state:"AR",lng:28.21,lat:94.72},
+        {state:"AS",lng:26.20,lat:92.93},
+        {state:"BR",lng:25.09,lat:85.31},
+        {state:"CG",lng:21.27,lat:81.86},
+        {state:"GA",lng:15.29,lat:74.12},
+        {state:"GJ",lng:22.25,lat:71.19},
+        {state:"HR",lng:29.05,lat:76.08},
+        {state:"HP",lng:31.10,lat:77.17},
+        {state:"JK",lng:33.77 ,lat:76.57},
+        {state:"JH",lng:23.61,lat:85.27},
+        {state:"KA",lng:15.31,lat:75.71},
+        {state:"KL",lng:10.85,lat:76.27},
+        {state:"MP",lng:22.97,lat:78.65},
+        {state:"MH",lng:19.75,lat:75.71},
+        {state:"MN",lng:24.66,lat:93.90},
+        {state:"ML",lng:25.46,lat:91.36},
+        {state:"MZ",lng:23.16,lat:92.93},
+        {state:"NL",lng:26.15,lat:94.56},
+        {state:"OR",lng:20.95,lat:85.09},
+        {state:"PB",lng:31.14,lat:75.34},
+        {state:"RJ",lng:27.02,lat:74.21},
+        {state:"SK",lng:27.53,lat:88.51},
+        {state:"TN",lng:11.12,lat:78.65},
+        {state:"TR",lng:23.94,lat:91.98},
+        {state:"UT",lng:30.06,lat:79.01},
+        {state:"UP",lng:26.84,lat:80.94},
+        {state:"WB",lng:22.98,lat:87.85}]
+
 
     const [data, setData] = useState([])
 
     const getData = async () =>{
-       const res = await fetch('https://api.covid19india.org/data.json')
+       const res = await fetch('https://mysterious-headland-18062.herokuapp.com/https://data.covid19india.org/v4/min/data.min.json')
         const newdata = await res.json()
         console.log(newdata)
-        setData(newdata.statewise)
+        setData(newdata)
     }
 
     useEffect(() => {
@@ -65,64 +71,64 @@ const Data = () => {
 
         {
 
-            
-            data.map((ele, ind) => {
+            Object.entries(data).map(item => {
 
-                const latm = states.find(x => x.state === ele.state)?.lat
-                const lngm = states.find(x => x.state === ele.state)?.lng
-
-            if(ind < 1){ 
-               act = act + parseInt(ele.active);
-               rec = rec + parseInt(ele.recovered);
-               dec = dec + parseInt(ele.deaths);
-                }
-
+                conf = conf + parseInt(item["1"]["total"]["confirmed"])
+                rec = rec + parseInt(item["1"]["total"]["recovered"])
+                dec = dec + parseInt(item["1"]["total"]["deceased"])
 
             })
+
         }
 
-        <Card
-         active ={act}
-         rec = {rec}
-         dec = {dec}
+
+
+         <Card
+         confirmed ={conf}
+         rec = {rec/2}
+         dec = {dec/2}
         />
 
-                        <ReactMapGL 
+             <ReactMapGL  
                 {...viewport}
                 mapboxApiAccessToken="pk.eyJ1IjoiaWFtZWthZ3JhIiwiYSI6ImNrcXhjczljcDB3d2ozMXFweGhkZXdzOGUifQ.Bg1hRtu-lGoR6O79m8wJow"
                 onViewportChange={viewport => {
                     setViewport(viewport);
-                }}
-                  >
+                }} >     
 
-                  {
+               {
 
-                      
-            
-            data.map((ele, ind) => {
+            Object.entries(data).map(item => {
 
-const latm = states.find(x => x.state === ele.state)?.lat;
-const lngm = states.find(x => x.state === ele.state)?.lng;
 
-if(latm!==undefined && lngm!==undefined){
+                    const latm = states.find(x => x.state === item["0"])?.lat;
+                    const lngm = states.find(x => x.state === item["0"])?.lng;
+
+        
+            if(latm!==undefined && lngm!==undefined){
+
+
+
     return (
+
 
 <Marker latitude={lngm} longitude={latm}>
        <div className="state"
-       >{ele.active} active
+       >{item["1"]["delta"]["confirmed"]} today
        </div>
      </Marker>
 
 )
-}
 
+}
 
 })
 
 }
 
-        </ReactMapGL>
+        </ReactMapGL> 
 
+   
 
         </div>
 
